c2e9b794ef26bcc2f44302ca7a822334c2d3f02f
Brian Wing <bwing@anaconda.com>
2025-09-10 09:45:04 -0400

    Added docstring to add() function

--- 1_library_update/lib-v1/include/calculator.h
+++ 1_library_update/lib-v1/include/calculator.h
@@ -6,6 +6,14 @@ public:
     Calculator();
     ~Calculator();
 
+    /**
+     * Adds two floating point values and returns their sum. Additionally, the result of the last add() call is saved
+     * in lastResult.
+     *
+     * @param a The first value to add.
+     * @param b The second value to add.
+     * @return The sum of the two values.
+     */
     float add(float a, float b);
 
     char version[16];
--- 1_library_update/lib-v2/include/calculator.h
+++ 1_library_update/lib-v2/include/calculator.h
@@ -6,6 +6,15 @@ public:
     Calculator();
     ~Calculator();
 
+    /**
+     * Adds two floating point values and returns their sum. Additionally, the result of the last add() call is saved
+     * in lastResult.
+     *
+     * @param a The first value to add.
+     * @param b The second value to add.
+     * @param precision The number of decimal places to return.
+     * @return The sum of the two values rounded to the nearest place specified by precision.
+     */
     float add(float a, float b, int precision);
 
     char version[16];
